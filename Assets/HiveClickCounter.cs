befe772d9c552845937c4a7b472b7036e60d786e
Plajdo <31418257+Plajdo@users.noreply.github.com>
2018-02-22 21:08:17 +0100

    Unborn HEAD
    CANNOT PUBLISH

---
+++ Assets/HiveClickCounter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HiveClickCounter : MonoBehaviour {
+
+	public Text guiText;
+	public Button hiveButton;
+	private int clickNum = 0;
+
+	void Start(){
+		hiveButton.onClick.AddListener(addNumber);
+	}
+
+	void addNumber(){
+		clickNum = Int32.Parse(guiText.text);
+		clickNum = clickNum + 1;
+		guiText.text = clickNum.ToString();
+	}
+
+}
---
+++ Assets/MoneySell.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoneySell : MonoBehaviour {
+
+	public Text honeyCount;
+	public Text moneyCount;
+	public Button dollarButton;
+	private static int honeyAmount = 0;
+	private static int moneyAmount = 0;
+
+	void Start(){
+		dollarButton.onClick.AddListener(sellHoney);
+	}
+
+	void sellHoney(){
+		honeyAmount = Int32.Parse(honeyCount.text);
+		moneyAmount = moneyAmount + honeyAmount / 10;
+		honeyCount.text = "0";
+		moneyCount.text = moneyAmount.ToString();
+	}
+
+}
