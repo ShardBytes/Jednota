d5176a8d12003e54a51a3399aa5c8b1ae420ea83
Plasmoxy <sebohacker@gmail.com>
2018-02-28 17:47:34 +0100

    finnaly a nice script

--- SampleGame/Assets/TouchController.cs
+++ SampleGame/Assets/TouchController.cs
@@ -1,29 +1,67 @@
 ﻿/*
  * Single touch controller script by Plasmoxy
- * 
+ * This script is BAD. Dont use it even if your life depended on it.
  * */
 
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [DisallowMultipleComponent]
 public class TouchController : MonoBehaviour {
 
-	public GameObject cube, torus;
+	public GameObject cube, torus, counterText;
 	public Material mat_a, mat_b;
+
 	private bool touched;
 
-	private long cubeIterator;
+	private long cubeCount;
 
 	private GameObject getCurrentCube()
 	{
-		return GameObject.Find("Cube#" + cubeIterator.ToString());
+		return GameObject.Find("Cube#" + cubeCount.ToString());
 	}
 
 	private Vector3 getPosByTouch(Touch t)
 	{
 		return Camera.main.ScreenToWorldPoint(new Vector3(t.position.x + Screen.width / 10, t.position.y, 10));
+	}
+
+	private void processTouch(Touch t) // probably should rewrite it through switch !
+	{
+		Vector3 touchPos = getPosByTouch(t);
+		if (t.phase == TouchPhase.Began && !touched)
+		{
+			Debug.Log("TOUCH");
+			touched = true;
+			cubeCount++;
+			Vector3 newPos = getPosByTouch(t);
+			newPos.z = 0; // fix z pos
+			if (touchPos.y > torus.transform.position.y) // if its higher than the torus
+			{
+				GameObject newCube = Instantiate(cube, newPos, Quaternion.identity);
+				newCube.name = "Cube#" + cubeCount.ToString();
+				newCube.GetComponent<Renderer>().material = Random.value > 0.5f ? mat_a : mat_b;
+			}
+		}
+		else if (t.phase == TouchPhase.Moved && touched && touchPos.y > torus.transform.position.y)
+		{
+			Debug.Log("MOVE");
+			Vector3 newPos = touchPos;
+			newPos.z = 0;
+			getCurrentCube().transform.position = newPos;
+		}
+		else if ((t.phase == TouchPhase.Ended || t.phase == TouchPhase.Canceled) && touched && touchPos.y > torus.transform.position.y)
+		{
+			Debug.Log("RELEASE");
+			touched = false;
+			GameObject c = getCurrentCube();
+			if (c == null) return; // totally hardcoded null reference bugfix, need to rewrite the whole script but good for a begginer lol
+			Debug.Log("CREATED " + c.name);
+			c.GetComponent<Rigidbody>().useGravity = true; // enable gravity after releasing cube
+			counterText.GetComponent<TextMesh>().text = cubeCount.ToString();
+		}
 	}
 
 	void Start () {}
@@ -32,33 +70,7 @@ public class TouchController : MonoBehaviour {
 
 		if (Input.touchCount > 0)
 		{
-			Touch t = Input.GetTouch(0);
-			if (t.phase == TouchPhase.Began && !touched)
-			{
-				touched = true;
-				Vector3 newPos = getPosByTouch(t);
-				newPos.z = 0; // fix z pos
-				if (newPos.y > torus.transform.position.y) // if its higher than the torus
-				{
-					GameObject newCube = Instantiate(cube, newPos, Quaternion.identity);
-					newCube.name = "Cube#" + cubeIterator.ToString();
-					newCube.GetComponent<Renderer>().material = Random.value > 0.5f ? mat_a : mat_b;
-				}
-			}
-			else if (t.phase == TouchPhase.Moved && touched)
-			{
-				Vector3 newPos = getPosByTouch(t);
-				newPos.z = 0;
-				getCurrentCube().transform.position = newPos;
-			}
-			else if ( (t.phase == TouchPhase.Ended || t.phase == TouchPhase.Canceled) && touched)
-			{
-				GameObject c = getCurrentCube();
-				Debug.Log("CREATED " + c.name);
-				c.GetComponent<Rigidbody>().useGravity = true; // enable gravity after releasing cube
-				cubeIterator++;
-				touched = false;
-			}
+			processTouch(Input.GetTouch(0));
 		}
 	}
 }
